d65b1c0c5e0fceeb1c5cdea434d540218b6c4b22
yvan-allioux <yvan.allioux.pro@gmail.com>
2021-11-23 17:51:39 +0100

    modification de la recherche
    pb modulo

--- main.c
+++ main.c
@@ -2,13 +2,23 @@
 
 int main()
 {
-    int nb = 1;
-    while(nb != 101){
+    int nb = 3;
+    while(nb != 10){
     //printf(nb,"\n");
-    printf("%d\n", nb);
-    //while(){
+    printf("%d\n-nb-", nb);
+
+    int varTest = nb-1;
+    printf("%d\n-modulo-", nb%varTest);
+
+    do{
+        varTest--;
+        if(nb%varTest != 0){
+            printf("%d\n-premier avec-", varTest);
+            varTest = 1;
+        }
         
-    //}
+    }while(nb%varTest != 0);
+
     nb++;
     }
 
